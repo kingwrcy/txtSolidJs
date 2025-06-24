a128989050bb63b259acf082eceaf8149840d02c
kingwrcy <gaeidy@gmail.com>
2025-06-24 14:57:52 +0800

    add deploy.yaml

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
       script: 'dist/index.js',
       env: {
         NODE_ENV: 'production',
-        DB_FILE_NAME: 'data.db',
+        DB_FILE_NAME: '/var/www/txtSolidJs/data.db',
         PORT: 36391,
       }
     }
